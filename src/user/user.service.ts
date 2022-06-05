0083a588bcbd99496a2b25543a3996f3d7527735
astolfovalentim <astolfom@gmail.com>
2022-06-05 11:15:36 -0300

    Senha segura

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,54 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { handleError } from 'src/utils/handle-error.util';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UserService {
+  private userSelect = {
+    id: true,
+    name: true,
+    email: true,
+    password: false,
+    cpf: true,
+    isAdmin: true,
+    createdAt: true,
+    updatedAt: true,
+  };
   constructor(private readonly prisma: PrismaService) {}
 
-  create(createUserDto: CreateUserDto) {
-    const data: User = { ...createUserDto };
+  async create(createUserDto: CreateUserDto) {
+    if (createUserDto.password != createUserDto.confirmPassword) {
+      throw new BadRequestException('As senhas informadas não são iguais.');
+    }
 
-    return this.prisma.user.create({ data }).catch(handleError);
+    delete createUserDto.confirmPassword;
+
+    const data: User = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10),
+    };
+
+    return this.prisma.user
+      .create({ data, select: this.userSelect })
+      .catch(handleError);
   }
 
   findAll() {
-    return this.prisma.user.findMany();
+    return this.prisma.user.findMany({ select: this.userSelect });
   }
 
   async findById(id: string): Promise<User> {
-    const record = await this.prisma.user.findUnique({ where: { id } });
+    const record = await this.prisma.user.findUnique({
+      where: { id },
+      select: this.userSelect,
+    });
 
     if (!record) {
       throw new NotFoundException(`Registro com o ID '${id}' não encontrado`);
@@ -35,11 +63,24 @@ export class UserService {
 
   async update(id: string, dto: UpdateUserDto): Promise<User> {
     await this.findById(id);
+
+    if (dto.password != dto.confirmPassword) {
+      throw new BadRequestException('As senhas informadas não são iguais.');
+    }
+
+    delete dto.confirmPassword;
+
     const data: Partial<User> = { ...dto };
+
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, 10);
+    }
+
     return this.prisma.user
       .update({
         where: { id },
         data,
+        select: this.userSelect,
       })
       .catch(handleError);
   }
